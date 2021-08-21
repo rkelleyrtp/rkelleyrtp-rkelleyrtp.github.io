8e5fbe663fa74e0667e723c7a8b0db0fb0860064
Ron Kelley <rkelleyrtp@gmail.com>
2021-08-21 16:29:57 -0400

    rebuilding site Sat 21 Aug 2021 04:29:57 PM EDT

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
 SOURCE_DIR="$HOME/nc.rkelleyrtp.com/RKelleyRTP_Web_Site/hugo-rkelleyrtp"
-  DEST_DIR="$HOME/nc.rkelleyrtp.com/RKelleyRTP_Web_Site/rkelleyrtp-website/docs"
+  DEST_DIR="$HOME/nc.rkelleyrtp.com/RKelleyRTP_Web_Site/rkelleyrtp-website"
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project.
-RES=$(cd $SOURCE_DIR &&  hugo -d $DEST_DIR)
+RES=$(cd $SOURCE_DIR &&  hugo -d $DEST_DIR/docs)
 if [ $? -eq 0 ];  then
-  cd ../rkelleyrtp-website 
 
-  git add docs
+  git add .
   [[ $? -ne 0 ]] && echo "Unable to add docs to git ; exiting " && exit 1
 
    git commit -m "$(echo "rebuilding site `date`")"
